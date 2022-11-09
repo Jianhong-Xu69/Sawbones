5f54d442319b3f4e6aeb24e7a04a72e495250a79
jianx <jxu5406@bths.edu>
2022-11-09 17:55:28 -0500

    Patchnotes v0.03
    Added more functionality to RollDice()
    Now takes 3 ints, roll a z sided dice y times, in x sets

--- src/SawbonesDice.java
+++ src/SawbonesDice.java
@@ -4,18 +4,26 @@ public class SawbonesDice {
     {
     }
 
-    public String RollDice(int number, int dice)
+    public String RollDice(int times, int number, int dice)
     {
         int roll = 0;
+        int each = 0;
         int total = 0;
         String totalRoll = "";
-        for (int i = 0; i < number; i++)
+        for (int i = 0; i < times; i++)
         {
-            roll = (int) ((Math.random() * dice) + 1);
-            total += roll;
-            totalRoll += roll + ", ";
+            totalRoll += "Set " + (i + 1) + " out of " + times + ": ";
+            for (int d = 0; d < number; d++)
+            {
+                roll = (int) ((Math.random() * dice) + 1);
+                each += roll;
+                totalRoll += roll + ", ";
+            }
+            totalRoll += "for a total of " + each + "\n";
+            total += each;
+            each = 0;
         }
-        totalRoll += "for a total of " + total;
+        totalRoll += "The total of all these rolls is: " + total;
         return totalRoll;
     }
 }
--- src/SawbonesRunner.java
+++ src/SawbonesRunner.java
@@ -1,6 +1,6 @@
 public class SawbonesRunner {
     public static void main(String[] args) {
         SawbonesDice test = new SawbonesDice();
-        System.out.print(test.RollDice(4,6));
+        System.out.print(test.RollDice(6,2,4));
     }
 }
