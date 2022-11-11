4529f0c043b65ef8db3ef1c91c44fecde90aa7fb
jianx <jxu5406@bths.edu>
2022-11-11 13:33:23 -0500

    Patchnotes 0.05
    Added more variables to SawbonesCharacter.java
    inventory
    proficiencies
    skills
    hitPoints
    stamina
    
    In SawbonesCharacter.java:
    
    Changed SawbonesCharacter() to be a 4 parameter constructor, removing specialisation fill. Testing RogueSetup() call
    
    Added black SawbonesCharacter() constructor, default constructor with no parameters
    
    Changed ChangeStats() to increment stats instead of setting them to a value
    
    Added RogueSetup() method, a void that sets up the starting loadout of a rogue
    
    Removed SetStats() method, ended up not having a use
    
    Edited Planning Document

--- src/SawbonesCharacter.java
+++ src/SawbonesCharacter.java
@@ -3,84 +3,106 @@ public class SawbonesCharacter {
     private String classes = "";
     private String specialisation = "";
     private String race = "";
-    private int level = 0;
     private String features = "";
-    private int strength;
-    private int agility;
-    private int endurance;
-    private int intellect;
-    private int wisdom;
-    private int charisma;
+    private String inventory = "";
+    private String proficiencies = "";
+    private String skills = "";
+    private int level = 0;
+    private int hitPoints = 0;
+    private int stamina = 0;
+    private int strength = 0;
+    private int agility = 0;
+    private int endurance = 0;
+    private int intellect = 0;
+    private int wisdom = 0;
+    private int charisma = 0;
 
-    public SawbonesCharacter(String name, String classes, String specialisation, String race, int level)
+    SawbonesDice roller = new SawbonesDice();
+    public SawbonesCharacter()
+    {
+        name = "nameless";
+        classes = "none";
+        race = "human";
+        level = 1;
+    }
+    public SawbonesCharacter(String name, String classes, String race, int level)
     {
         this.name = name;
-        this.classes = classes;
-        this.specialisation = specialisation;
-        this.race = race;
         this.level = level;
-        if (classes.toLowerCase().equals("rogue"))
+        if (classes.equalsIgnoreCase("rogue"))
         {
-            features += "Gang Up\nBanditry\n";
-        }
-        else if (classes.toLowerCase().equals("runemage"))
-        {
-            features += "Runecasting\nEvocation\n";
-        }
-        else if (classes.toLowerCase().equals("knight"))
-        {
-            features += "Defender\nValor\n";
-        }
-        else if (classes.toLowerCase().equals("monk"))
-        {
-            features += "Qi\nStances\n";
+            RogueSetup();
         }
     }
 
-    public void SetStats(int str, int dex, int con, int intel, int wis, int cha)
-    {
-        strength = str;
-        agility = dex;
-        endurance = con;
-        intellect = intel;
-        wisdom = wis;
-        charisma = cha;
-    }
     public String ChangeStats(int stat, int number)
     {
         if (stat == 1)
         {
-            strength = number;
+            strength += number;
             return "Strength is now set to " + strength;
         }
         else if (stat == 2)
         {
-            agility = number;
+            agility += number;
             return "Dexterity is now set to " + agility;
         }
         else if (stat == 3)
         {
-            endurance = number;
+            endurance += number;
             return "Constitution is now set to " + endurance;
         }
         else if (stat == 4)
         {
-            intellect = number;
+            intellect += number;
             return "Intelligence is now set to " + intellect;
         }
         else if (stat == 5)
         {
-            wisdom = number;
+            wisdom += number;
             return "Wisdom is now set to " + wisdom;
         }
         else if (stat == 6)
         {
-            charisma = number;
+            charisma += number;
             return "Charisma is now set to " + charisma;
         }
         else
         {
             return "That isn't a valid stat!";
+        }
+    }
+    public void RogueSetup()//DONE (?)
+    {
+        classes = "Rogue";
+        features = "Gang Up\nBanditry";
+        proficiencies = "Light Armour\nTwo from Axes, Bludgeons, Daggers, Crossbows, Bows, and Firearms.\n";
+        inventory = "Choose between:\n(A) A Hand Axe and Tomahawk\n(B) Two Shivs\n(C) A Club, A Flintlock Pistol, and 30 Handgun Ammunition\n(D) A Hand Crossbow, 30 Hand Crossbow Bolts and a Knife\n(E) A Dirk, A Shortbow, and 30 arrows\nOne set of hide armor, a bag of tricks, three day's worth of provisions, twenty lock picks, a hood, and a mask or cloth scarf. ";
+        skills = "Choose four from: Subterfuge\nStealth\nDodge\nLight Weapons\nInvestigation\nIntimidation\nArchery\nPersuasion\nPerception\nIncrease them by 3";
+    }
+    public void RollHealthStamina()//NOT DONE NEED OTHER 3 CLASSES
+    {
+        if (classes.equalsIgnoreCase("rogue"))
+        {
+            for (int i = 1; i < level; i++)
+            {
+                hitPoints += Integer.parseInt(roller.RollDice(1,1,6, (endurance/2)));
+            }
+        }
+        if (classes.equalsIgnoreCase("knight"))
+        {
+            for (int i = 1; i < level; i++)
+            {
+                hitPoints += Integer.parseInt(roller.RollDice(1,1,6, (endurance/2)));
+            }
+        }
+        if (endurance > 0)
+        {
+            stamina = 3 + endurance;
+        }
+        else
+        {
+            stamina = 3;
         }
     }
     public String CharacterInformation()
--- src/SawbonesRunner.java
+++ src/SawbonesRunner.java
@@ -1,32 +1,42 @@
 import java.util.Scanner;
 public class SawbonesRunner {
     public static void main(String[] args) {
+        System.out.println("This character creator is built for Sawbones 1e, NOT D&D 5e");
+        System.out.println("This is not meant to be a replacement to a character sheet");
+        System.out.println("This was made because creating a character takes a hilarious amount of time");
         Scanner user = new Scanner(System.in);
         SawbonesDice test = new SawbonesDice();
-        System.out.print("What is your character's name? ");
-        String name = user.nextLine();
-        System.out.print("What is your character's class? ");
-        String classes = user.nextLine();
-        System.out.print("What is your character's specialisation? ");
-        String special = user.nextLine();
-        System.out.print("What is your character's race? ");
-        String race = user.nextLine();
-        System.out.print("What level is your character? ");
-        int level = user.nextInt();
-        SawbonesCharacter adventurer = new SawbonesCharacter(name, classes, special, race, level);
-        System.out.println(test.RollDice(2,6,8, -4));
-        System.out.print("Input your Strength stat: ");
-        adventurer.ChangeStats(1, user.nextInt());
-        System.out.print("Input your Agility stat: ");
-        adventurer.ChangeStats(2, user.nextInt());
-        System.out.print("Input your Endurance stat: ");
-        adventurer.ChangeStats(3, user.nextInt());
-        System.out.print("Input your Intellect stat: ");
-        adventurer.ChangeStats(4, user.nextInt());
-        System.out.print("Input your Wisdom stat: ");
-        adventurer.ChangeStats(5, user.nextInt());
-        System.out.print("Input your Charisma stat: ");
-        adventurer.ChangeStats(6, user.nextInt());
-        System.out.println(adventurer.CharacterInformation());
+        System.out.println("Do you want to create a blank character sheet?");
+        if (user.nextLine().toLowerCase().equals("yes"))
+        {
+            SawbonesCharacter blank = new SawbonesCharacter();
+        }
+        else
+        {
+            System.out.print("What is your character's name? ");
+            String name = user.nextLine();
+            System.out.print("What is your character's class? ");
+            String classes = user.nextLine();
+            System.out.print("What is your character's race? ");
+            String race = user.nextLine();
+            System.out.print("What level is your character? ");
+            int level = user.nextInt();
+            SawbonesCharacter adventurer = new SawbonesCharacter(name, classes, race, level);
+            System.out.println(test.RollDice(2, 6, 8, -4));
+            System.out.print("Input your Strength stat: ");
+            System.out.println(adventurer.ChangeStats(1, user.nextInt()));
+            System.out.print("Input your Agility stat: ");
+            System.out.println(adventurer.ChangeStats(2, user.nextInt()));
+            System.out.print("Input your Endurance stat: ");
+            System.out.println(adventurer.ChangeStats(3, user.nextInt()));
+            System.out.print("Input your Intellect stat: ");
+            System.out.println(adventurer.ChangeStats(4, user.nextInt()));
+            System.out.print("Input your Wisdom stat: ");
+            System.out.println(adventurer.ChangeStats(5, user.nextInt()));
+            System.out.print("Input your Charisma stat: ");
+            System.out.println(adventurer.ChangeStats(6, user.nextInt()));
+            adventurer.RollHealthStamina();
+            System.out.println(adventurer.CharacterInformation());
+        }
     }
 }
